d576bbe86a8467de5e5a2e0330e9b984568fa70c
Fabricio-Ajala <fabricio_ajala@hotmail.com>
2022-06-07 11:29:17 -0300

    fix(ImgUrl.java) ajustando o forms para receber um vetor de Strings

--- src/main/java/sptech/bentscadastro/restaurant/form/ImgUrl.java
+++ src/main/java/sptech/bentscadastro/restaurant/form/ImgUrl.java
@@ -1,17 +1,28 @@
 package sptech.bentscadastro.restaurant.form;
 
 public class ImgUrl {
-    private String imgUrl;
+    private String[] imgUrl;
+    private int size;
 
-    public ImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
+    public ImgUrl(String[] imgUrl) {
+        this.imgUrl = new String[5];
+        size = 0;
     }
 
     public String getImgUrl() {
-        return imgUrl;
+        return imgUrl[size];
     }
 
     public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
+        this.imgUrl[size] = imgUrl;
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 }
