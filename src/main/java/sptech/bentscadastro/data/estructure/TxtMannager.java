3a5098d32c55cf26050aa5d2cf675646053c4695
Fabricio-Ajala <fabricio_ajala@hotmail.com>
2022-06-13 05:26:43 -0300

    feat(TxtMannager.java) adicionando metodo para o registro de novos restaurantes

--- src/main/java/sptech/bentscadastro/data/estructure/TxtMannager.java
+++ src/main/java/sptech/bentscadastro/data/estructure/TxtMannager.java
@@ -4,6 +4,8 @@ import net.bytebuddy.asm.Advice;
 import org.springframework.web.multipart.MultipartFile;
 import sptech.bentscadastro.menufood.entity.MenuFood;
 import sptech.bentscadastro.menufood.form.UpdateItemMenuFoodForm;
+import sptech.bentscadastro.restaurant.entity.Restaurant;
+import sptech.bentscadastro.user.entity.User;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,10 +13,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class TxtMannager {
 
-    public List<MenuFood> txtReaderMenuFood(String nomeArq) {
+    public List<MenuFood> txtReaderMenuFood(String fileName) {
 
         BufferedReader entrada = null;
 
@@ -28,7 +31,7 @@ public class TxtMannager {
         // Abre o arquivo para leitura
 
         try {
-            entrada = new BufferedReader(new FileReader(nomeArq));
+            entrada = new BufferedReader(new FileReader(fileName));
         } catch (IOException erro) {
             System.out.println("Erro ao abrir o arquivo: " + erro);
         }
@@ -66,7 +69,7 @@ public class TxtMannager {
 
 
                 } else {
-                    System.out.println("Registro Invalido!");
+                    System.out.println("Registro Diferente de Cardapio!");
                 }
                 registro = entrada.readLine();
             }
@@ -78,4 +81,108 @@ public class TxtMannager {
         return listFood;
 
     }
+
+    public Queue txtReaderNewUser(String filename) {
+
+        BufferedReader entrada = null;
+
+        Stack stackUser = new Stack(20);
+        Stack stackRestaurant = new Stack(20);
+
+        Queue queueList = new Queue<>(40);
+
+        String registro, tipoRegistro;
+
+        // User
+        String nameUser, password, typeUser, emailUser, cep, phoneUser, state, city, district, adress, adressNumber;
+        Float latitude, longitude;
+
+        // Restaurant
+
+
+        // Abre o arquivo para leitura
+
+        try {
+            entrada = new BufferedReader(new FileReader(filename));
+        } catch (IOException erro) {
+            System.out.println("Erro ao abrir o arquivo: " + erro);
+        }
+
+        // Leitura arquivo
+
+        try {
+
+            registro = entrada.readLine(); // Lê o primeiro registro do arquivo
+
+            while (registro != null) { // Enquanto não for o final do arquivo
+
+                tipoRegistro = registro.substring(0, 2); // Obtem os 2 primeiros caracteres do registro
+
+               if (tipoRegistro.equals("01")) {
+
+                    // Trim elimina os brancos à direita da String
+
+                    password = registro.substring(2,37).trim();
+                    typeUser = registro.substring(37,47).trim();
+                    nameUser = registro.substring(47, 82).trim();
+                    emailUser = registro.substring(82,142).trim();
+                    phoneUser = registro.substring(142,157).trim();
+                    cep = registro.substring(157,165).trim();
+                    state = registro.substring(165,195).trim();
+                    city = registro.substring(195,245).trim();
+                    district = registro.substring(245,295).trim();
+                    adress = registro.substring(295,375).trim();
+                    adressNumber = registro.substring(375,380).trim();
+                    latitude = Float.valueOf(registro.substring(380,388).replace(",", "."));
+                    longitude = Float.valueOf(registro.substring(388,396).replace(",", "."));
+
+                    System.out.println("E um registro de Corpo de Restaurante!");
+                    System.out.println("Tipo: " + typeUser);
+                    System.out.println("Nome do Restaurante: " + nameUser);
+                    System.out.println("Email do Restaurante: " + emailUser);
+                    System.out.println("Senha do Restaurante:" + password);
+                    System.out.println("Telefone: " + phoneUser);
+                    System.out.println("CEP: " + cep);
+                    System.out.println("Estado: " + state);
+                    System.out.println("Cidade: " + city);
+                    System.out.println("Distrito: " + district);
+                    System.out.println("Endereço: " + adress);
+                    System.out.println("Número Endereço: " + adressNumber);
+                    System.out.println("Latitude: " + latitude);
+                    System.out.println("Longitude: " + longitude);
+
+                    User newUser = new User();
+                    Restaurant newRestaurant = new Restaurant();
+
+                    newUser.setName(nameUser);
+                    newUser.setEmail(emailUser);
+                    newUser.setPassword(password);
+                    newUser.setUserType(typeUser);
+                    newUser.setPhone(phoneUser);
+                    newUser.setCep(cep);
+                    newUser.setState(state);
+                    newUser.setCity(city);
+                    newUser.setDistrict(district);
+                    newUser.setAddress(adress);
+                    newUser.setAddressNumber(adressNumber);
+                    newUser.setLat(latitude);
+                    newUser.setLng(longitude);
+
+
+
+                }
+                else {
+                    System.out.println("Registro Diferente de Usuário!");
+                }
+                // Ler o próximo registro!
+                registro = entrada.readLine();
+            }
+            entrada.close();
+        } catch (IOException erro) {
+            System.out.println("Erro ao ler o tipo de registro: " + erro);
+        }
+
+        return queueList;
+    }
+
 }
