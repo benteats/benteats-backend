d9d4550cd45278617c1c09b85d682f119f73f64f
eliasnachle <elias.nachle@bandtec.com.br>
2022-06-14 16:15:15 -0300

    fix: corrigindo origin

--- src/main/java/sptech/bentscadastro/user/security/JWTConfiguration.java
+++ src/main/java/sptech/bentscadastro/user/security/JWTConfiguration.java
@@ -51,7 +51,7 @@ public class JWTConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://benteats.vercel.app"));
         corsConfiguration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
